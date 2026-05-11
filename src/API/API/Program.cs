522a63830ca0aab7ac994ebb656ea3f79adf524b
Pedro Accoroni delarcina <pedro.delarcina@comlinksa.com>
2026-05-11 16:40:17 -0300

    criado Dockerfile do front e integrado ao compose

--- src/API/API/Program.cs
+++ src/API/API/Program.cs
@@ -56,14 +56,11 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy.WithOrigins(
-                "http://localhost:4200",
-                "https://localhost:4200",
-                "http://localhost:5173",
-                "https://localhost:5173",
-                "http://localhost:8080")
+                "*")
               .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyOrigin();
+        //  .AllowCredentials();
     });
 });
 
